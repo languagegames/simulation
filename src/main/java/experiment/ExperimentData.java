e9b6e2eaf9e729a908950462aa1b7212367498c7
Paddy <pemcgovern@gmail.com>
2015-02-03 15:43:52 +0000

    Implement ExperimentData.

--- src/main/java/experiment/ExperimentData.java
+++ src/main/java/experiment/ExperimentData.java
@@ -1,13 +1,42 @@
 package experiment;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import conceptualspace.Material;
 
 public class ExperimentData {
 
+	private final List<Material> materials = new ArrayList<>();
+	private final List<Material> trainingSet;
+	private final List<Material> testSet;
+
 	public ExperimentData(final List<Material> materials, final double trainTestSplit) {
-		// TODO Auto-generated constructor stub
+		this.materials.addAll(materials);
+		final int trainingSetSize = (int) (trainTestSplit * materials.size());
+		trainingSet = materials.subList(0, trainingSetSize);
+		testSet = materials.subList(trainingSetSize, materials.size());
+	}
+
+	public List<Material> trainingSet() {
+		return trainingSet;
+	}
+
+	public List<Material> testSet() {
+		return testSet;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
 	}
 
 }
---
+++ src/test/java/experiment/TestExperimentData.java
@@ -0,0 +1,27 @@
+package experiment;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+
+import org.junit.Test;
+
+import conceptualspace.Material;
+import conceptualspace.Point;
+import conceptualspace.SimpleMaterial;
+
+public class TestExperimentData {
+
+	private final Material someMaterial = new SimpleMaterial(new Point(0.42));
+	private final Material material0 = someMaterial, material1 = someMaterial,
+			material2 = someMaterial, material3 = someMaterial;
+
+	@Test
+	public void splitsDataAccordingToSpecifiedProportion() {
+		final ExperimentData data = new ExperimentData(asList(material0, material1, material2, material3), 0.75);
+
+		assertThat(data.trainingSet(), contains(material0, material1, material2));
+		assertThat(data.testSet(), contains(material3));
+	}
+
+}
