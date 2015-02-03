9ddd829f3a8ba6e66cd61105c68b6d08c0cc826c
Paddy <pemcgovern@gmail.com>
2015-02-03 15:32:41 +0000

    Implement Point and SimpleMaterial.

--- src/main/java/conceptualspace/Point.java
+++ src/main/java/conceptualspace/Point.java
@@ -1,9 +1,34 @@
 package conceptualspace;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class Point {
 
+	private final List<Double> coordinates = new ArrayList<>();
+
 	public Point(final double...coordinates) {
-		// TODO Auto-generated constructor stub
+		for (final double coordinate : coordinates) {
+			this.coordinates.add(coordinate);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return coordinates.toString();
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
 	}
 
 }
--- src/main/java/conceptualspace/SimpleMaterial.java
+++ src/main/java/conceptualspace/SimpleMaterial.java
@@ -1,10 +1,30 @@
 package conceptualspace;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 
 public class SimpleMaterial implements Material {
 
+	private final Point point;
+
 	public SimpleMaterial(final Point point) {
-		// TODO Auto-generated constructor stub
+		this.point = point;
+	}
+
+	@Override
+	public String toString() {
+		return "Simple material @ " + point.toString();
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
 	}
 
 }
