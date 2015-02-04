44334036af6b684bc2923d2aea027c202bea0cb8
Paddy <pemcgovern@gmail.com>
2015-02-04 10:20:32 +0000

    Implement basics of Interpretation and Concept.

--- src/main/java/agent/Interpretation.java
+++ src/main/java/agent/Interpretation.java
@@ -1,11 +1,36 @@
 package agent;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import conceptualspace.Concept;
 
 public class Interpretation {
 
+	private final List<Concept> concepts = new ArrayList<>();
+
 	public Interpretation(final Concept...concepts) {
-		// TODO Auto-generated constructor stub
+		for (final Concept concept : concepts) {
+			this.concepts.add(concept);
+		}
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public String toString() {
+		return concepts.toString();
 	}
 
 }
--- src/main/java/conceptualspace/Concept.java
+++ src/main/java/conceptualspace/Concept.java
@@ -1,9 +1,31 @@
 package conceptualspace;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class Concept {
 
+	private final Point prototype;
+	private final double threshold;
+
 	public Concept(final Point prototype, final double threshold) {
-		// TODO Auto-generated constructor stub
+		this.prototype = prototype;
+		this.threshold = threshold;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public String toString() {
+		return "Concept(" + prototype.toString() + ", " + threshold + ")";
 	}
 
 }
