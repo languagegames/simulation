00b681207c80b845c95591f840a19730ee50ed4b
Paddy <pemcgovern@gmail.com>
2015-02-04 10:28:20 +0000

    Implement basics in BasicAgent.

--- src/main/java/agent/BasicAgent.java
+++ src/main/java/agent/BasicAgent.java
@@ -1,5 +1,11 @@
 package agent;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import conceptualspace.Concept;
 import conceptualspace.Material;
 import experiment.Assertion;
@@ -7,8 +13,12 @@ import experiment.Assertion;
 
 public class BasicAgent implements Agent {
 
+	private final List<Concept> concepts = new ArrayList<>();
+
 	public BasicAgent(final Concept...concepts) {
-		// TODO Auto-generated constructor stub
+		for (final Concept concept : concepts) {
+			this.concepts.add(concept);
+		}
 	}
 
 	@Override
@@ -21,6 +31,21 @@ public class BasicAgent implements Agent {
 	public Agent learn(final Assertion label) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public String toString() {
+		return "Basic agent with " + concepts.toString();
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
 	}
 
 }
