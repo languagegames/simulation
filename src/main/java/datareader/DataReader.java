5064089fa19b4ed902ba15672ca8d40705358a12
Paddy <pemcgovern@gmail.com>
2015-02-05 15:03:13 +0000

    Implement points reading in DataReader.

--- src/main/java/datareader/DataReader.java
+++ src/main/java/datareader/DataReader.java
@@ -1,21 +1,55 @@
 package datareader;
 
+import static java.lang.Double.valueOf;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.nio.file.Files.readAllLines;
+import static java.nio.file.Paths.get;
+import static java.util.Arrays.asList;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 import conceptualspace.Point;
 
 public class DataReader {
 
-	public DataReader() {
-		// TODO Auto-generated constructor stub
+	public List<Point> points(final String file) {
+		final List<Point> points = new ArrayList<>();
+		try {
+			final List<String> lines = read(file);
+			for (final String line : lines) {
+				addNextPoint(points, line);
+			}
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+		return points;
 	}
 
-	public List<Point> points(final String fileName) {
-		// TODO Auto-generated method stub
-		return null;
+	private void addNextPoint(final List<Point> points, final String line) {
+		if (! line.isEmpty()) {
+			final List<String> lineStrings = new ArrayList<>();
+			lineStrings.addAll(asList(line.split(",")));
+			points.add(point(lineStrings));
+		}
 	}
 
-	public List<Integer> labels(final String fileName) {
+	private Point point(final List<String> lineStrings) {
+		final List<Double> coordinates = new ArrayList<>();
+		for (final String string : lineStrings) {
+			coordinates.add(valueOf(string));
+		}
+		return new Point(coordinates);
+	}
+
+	private List<String> read(final String file) throws IOException {
+		final Path path = get(file);
+		return readAllLines(path, UTF_8);
+	}
+
+	public List<Integer> labels(final String file) {
 		// TODO Auto-generated method stub
 		return null;
 	}
---
+++ src/test/java/datareader/TestDataReader.java
@@ -0,0 +1,23 @@
+package datareader;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+
+import org.junit.Test;
+
+import conceptualspace.Point;
+
+public class TestDataReader {
+
+	@Test
+	public void createsListOfPointsFromDataFile() {
+		final DataReader reader = new DataReader();
+		assertThat(reader.points("src/test/resources/testdata.csv"), contains(
+				point(0.1, 0.8), point(0.5, 0.5), point(0.4, 0.2), point(0.9, 0.8), point(0.7, 0.3)));
+	}
+
+	private Point point(final Double...coordinates) {
+		return new Point(coordinates);
+	}
+
+}
