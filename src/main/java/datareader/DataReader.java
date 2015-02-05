2986051d33af0b8495a06650e4ad5de223544ef9
Paddy <pemcgovern@gmail.com>
2015-02-05 14:32:56 +0000

    New DataReader class.

---
+++ src/main/java/datareader/DataReader.java
@@ -0,0 +1,23 @@
+package datareader;
+
+import java.util.List;
+
+import conceptualspace.Point;
+
+public class DataReader {
+
+	public DataReader(final String dataFile, final String labelFile) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<Point> points() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Integer> labels() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
