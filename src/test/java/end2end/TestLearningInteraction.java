3f086900926a45d7e30c524b0b483331d60a6924
Paddy <pemcgovern@gmail.com>
2015-02-05 14:33:13 +0000

    New end2end test for reading data from file.

--- src/test/java/end2end/TestLearningInteraction.java
+++ src/test/java/end2end/TestLearningInteraction.java
@@ -4,6 +4,8 @@ import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import agent.Agent;
@@ -14,10 +16,33 @@ import agent.OracleAgent;
 import conceptualspace.Material;
 import conceptualspace.Point;
 import conceptualspace.SimpleMaterial;
+import datareader.DataReader;
 import experiment.ClassificationExperiment;
 import experiment.ExperimentData;
 
 public class TestLearningInteraction {
+
+	private final Agent pupil = new BasicAgent(
+			0.42,
+			new Concept(new Point(0.3, 0.4), 1.0),
+			new Concept(new Point(0.6, 0.5), 0.7)
+			);
+
+	@Test
+	public void fiveObjectsTwoConceptsUsingDataFromFile() {
+
+		final DataReader dataReader = new DataReader("testdata", "testlabels");
+
+		final List<Material> materials = SimpleMaterial.makeListFrom(dataReader.points());
+		final ExperimentData data = new ExperimentData(materials, 0.8);
+		final List<Integer> labels = dataReader.labels();
+
+		final Agent teacher = new OracleAgent(new LabelMapping(data, labels), 0.95);
+
+		final ClassificationExperiment experiment = new ClassificationExperiment(data, pupil, teacher);
+
+		assertThat(experiment.classificationScore(), equalTo(1.0));
+	}
 
 	@Test
 	public void fiveObjectsTwoConceptsWithCommunicationGame() {
@@ -29,12 +54,6 @@ public class TestLearningInteraction {
 				simpleMaterial(new Point(0.9, 0.8)),
 				simpleMaterial(new Point(0.7, 0.3))),
 				0.8
-				);
-
-		final Agent pupil = new BasicAgent(
-				0.42,
-				new Concept(new Point(0.3, 0.4), 1.0),
-				new Concept(new Point(0.6, 0.5), 0.7)
 				);
 
 		final Agent teacher = new OracleAgent(new LabelMapping(data, asList(0, 1, 1, 0, 0)), 0.95);
