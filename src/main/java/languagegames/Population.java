328299e5234b2d7a398482308bdda57724cef302
Paddy <pemcgovern@gmail.com>
2015-02-10 15:32:12 +0000

    Implement convergence calculation in Population.

--- src/main/java/languagegames/Population.java
+++ src/main/java/languagegames/Population.java
@@ -1,18 +1,27 @@
 package languagegames;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import agent.Agent;
 
 public class Population {
 
+	private final List<Agent> agents = new ArrayList<>();
+
 	public Population(final List<Agent> agents) {
-		// TODO Auto-generated constructor stub
+		this.agents.addAll(agents);
 	}
 
 	public double convergence() {
-		// TODO Auto-generated method stub
-		return 0;
+		double result = 0;
+		final int numAgents = agents.size();
+		for (int i = 0; i < numAgents-1; i++) {
+			for (int j = i+1; j < numAgents; j++) {
+				result += agents.get(i).convergenceWith(agents.get(j)) / numAgents;
+			}
+		}
+		return result;
 	}
 
 }
---
+++ src/test/java/languagegames/TestPopulation.java
@@ -0,0 +1,35 @@
+package languagegames;
+
+import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.jmock.Expectations;
+import org.jmock.auto.Mock;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
+import org.junit.Test;
+
+import agent.Agent;
+
+public class TestPopulation {
+	@Rule public final JUnitRuleMockery context = new JUnitRuleMockery();
+
+	@Mock Agent agent0, agent1, agent2;
+
+	private final double result0 = 0.42, result1 = 0.43, result2 = 0.44;
+
+	@Test
+	public void getsAverageConvergenceAcrossAllPairsOfAgents() {
+		final Population population = new Population(asList(agent0, agent1, agent2));
+
+		context.checking(new Expectations() {{
+			oneOf(agent0).convergenceWith(agent1); will(returnValue(result0));
+			oneOf(agent0).convergenceWith(agent2); will(returnValue(result1));
+			oneOf(agent1).convergenceWith(agent2); will(returnValue(result2));
+		}});
+
+		assertThat(population.convergence(), equalTo((result0+result1+result2)/3));
+	}
+
+}
