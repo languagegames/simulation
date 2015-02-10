6a51d166529cbaa15fe6a1904ea8cbef704e8888
Paddy <pemcgovern@gmail.com>
2015-02-10 14:55:40 +0000

    Implement TimeSeries.

--- src/main/java/languagegames/analysis/TimeSeries.java
+++ src/main/java/languagegames/analysis/TimeSeries.java
@@ -1,9 +1,38 @@
 package languagegames.analysis;
 
+import static java.util.Arrays.asList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class TimeSeries {
 
-	public TimeSeries(final double...values) {
-		// TODO Auto-generated constructor stub
+	private final List<Double> values = new ArrayList<>();
+
+	public TimeSeries(final List<Double> values) {
+		this.values.addAll(values);
+	}
+
+	public TimeSeries(final Double...values) {
+		this(asList(values));
+	}
+
+	@Override
+	public String toString() {
+		return values.toString();
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
 	}
 
 }
