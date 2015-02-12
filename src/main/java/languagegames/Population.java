20d532f4f1ceb548cf268bb04f02ee397bf35e20
Paddy <pemcgovern@gmail.com>
2015-02-12 10:34:08 +0000

    Population runs language games.

--- src/main/java/languagegames/Population.java
+++ src/main/java/languagegames/Population.java
@@ -3,11 +3,17 @@ package languagegames;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 import agent.Agent;
+import agent.Assertion;
 
 public class Population {
 
 	private final List<Agent> agents = new ArrayList<>();
+	private final ObjectCreator objectCreator;
+	private final AgentPairer agentPairer;
 
 	public Population(
 			final List<Agent> agents,
@@ -15,6 +21,25 @@ public class Population {
 			final AgentPairer agentPairer)
 	{
 		this.agents.addAll(agents);
+		this.objectCreator = objectCreator;
+		this.agentPairer = agentPairer;
+	}
+
+	public Population runLanguageGames() {
+		final List<Integer> pairingOrder = agentPairer.pairingOrder(agents.size());
+		final List<Agent> updatedAgents = new ArrayList<>(agents);
+		for (int i=0; i<agents.size(); i+=2) {
+			final int speakerIndex = pairingOrder.get(i);
+			final int listenerIndex = pairingOrder.get(i+1);
+			final Agent updatedListener = languageGame(agents.get(speakerIndex), agents.get(listenerIndex));
+			updatedAgents.set(listenerIndex, updatedListener);
+		}
+		return new Population(updatedAgents, objectCreator, agentPairer);
+	}
+
+	private Agent languageGame(final Agent speaker, final Agent listener) {
+		final Assertion speakerAssertion = speaker.classify(objectCreator.create());
+		return listener.learn(speakerAssertion);
 	}
 
 	public double convergence() {
@@ -28,14 +53,24 @@ public class Population {
 		return sum / numberOfPairs;
 	}
 
-	public Population runLanguageGames() {
+	public Population incrementWeights(final double weightIncrement) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public Population incrementWeights(final double weightIncrement) {
-		// TODO Auto-generated method stub
-		return null;
+	@Override
+	public String toString() {
+		return agents.toString();
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
 	}
 
 }
--- src/test/java/languagegames/TestPopulation.java
+++ src/test/java/languagegames/TestPopulation.java
@@ -11,13 +11,40 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import agent.Agent;
+import agent.Assertion;
+import conceptualspace.PerceptualObject;
+import conceptualspace.Point;
+import conceptualspace.SimpleObject;
 
 public class TestPopulation {
 	@Rule public final JUnitRuleMockery context = new JUnitRuleMockery();
 
-	@Mock Agent agent0, agent1, agent2;
+	@Mock Agent agent0, agent1, agent2, agent3, updatedAgent0, updatedAgent3;
+	@Mock ObjectCreator objectCreator;
 
+	private final AgentPairer agentPairer = new StaticPairer(asList(2, 0, 1, 3));
 	private final double result0 = 0.42, result1 = 0.43, result2 = 0.44;
+
+	@Test
+	public void pairsOffAgentsAndGetsThemToDoLanguageGames() {
+		final Population population =
+				new Population(asList(agent0, agent1, agent2, agent3), objectCreator, agentPairer);
+
+		final PerceptualObject object = new SimpleObject(new Point(0.42));
+		final Assertion assertion0 = new Assertion(object, 42, 0.42);
+		final Assertion assertion1 = new Assertion(object, 43, 0.42);
+
+		context.checking(new Expectations() {{
+			exactly(2).of(objectCreator).create(); will(returnValue(object));
+			oneOf(agent2).classify(object); will(returnValue(assertion0));
+			oneOf(agent0).learn(assertion0); will(returnValue(updatedAgent0));
+			oneOf(agent1).classify(object); will(returnValue(assertion1));
+			oneOf(agent3).learn(assertion1); will(returnValue(updatedAgent3));
+		}});
+
+		assertThat(population.runLanguageGames(), equalTo(
+				new Population(asList(updatedAgent0, agent1, agent2, updatedAgent3), objectCreator, agentPairer)));
+	}
 
 	@Test
 	public void getsAverageConvergenceAcrossAllPairsOfAgents() {
