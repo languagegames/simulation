86e556cbd6d9948b9ce3a3554bcbf0b6c43b7204
Paddy <pemcgovern@gmail.com>
2015-02-12 14:53:43 +0000

    Fix bug in Simulation.

--- src/main/java/languagegames/Simulation.java
+++ src/main/java/languagegames/Simulation.java
@@ -7,19 +7,18 @@ import java.util.List;
 
 public class Simulation {
 
-	private final Population population;
+	private final List<Population> populations = new ArrayList<>();
 	private final double weightIncrement;
 
 	public Simulation(final Population population, final double weightIncrement) {
-		this.population = population;
+		populations.add(population);
 		this.weightIncrement = weightIncrement;
 	}
 
 	public SimulationHistory run(final int timeSteps) {
-		final List<Population> populations = new ArrayList<>();
-		populations.add(population);
 		for (int t = 0; t < timeSteps; t++) {
-			final Population next = population.runLanguageGames().incrementWeights(weightIncrement);
+			final Population current = populations.get(populations.size()-1);
+			final Population next = current.runLanguageGames().incrementWeights(weightIncrement);
 			populations.add(next);
 		}
 		return new SimulationHistory(populations);
