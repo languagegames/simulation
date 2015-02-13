ec8cb231d694ddd53aa18f02189bcfbffc335ca4
Paddy <pemcgovern@gmail.com>
2015-02-13 13:10:51 +0000

    Create OraclePopulation.

---
+++ src/main/java/languagegames/OraclePopulation.java
@@ -0,0 +1,36 @@
+package languagegames;
+
+import java.util.List;
+
+import agent.Agent;
+
+public class OraclePopulation implements Population {
+
+	public OraclePopulation(
+			final List<Agent> agents,
+			final List<Agent> oracles,
+			final ObjectPool objectCreator,
+			final AgentPairer staticPairer)
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public Population runLanguageGames() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double convergence() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Population incrementWeights(final double weightIncrement) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
