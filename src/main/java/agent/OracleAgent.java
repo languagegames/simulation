4428499d7398562678e942367855afa1c4d2fb6c
Paddy <pemcgovern@gmail.com>
2015-02-18 15:19:01 +0000

    Implement methods in OracleAgent.

--- src/main/java/agent/OracleAgent.java
+++ src/main/java/agent/OracleAgent.java
@@ -16,9 +16,8 @@ public class OracleAgent implements Agent {
 	}
 
 	@Override
-	public Agent incrementWeight(final double weightIncrement) {
-		// TODO Auto-generated method stub
-		return null;
+	public OracleAgent incrementWeight(final double weightIncrement) {
+		return this;
 	}
 
 	@Override
@@ -32,21 +31,18 @@ public class OracleAgent implements Agent {
 	}
 
 	@Override
-	public Agent learn(final Assertion assertion) {
-		// TODO Auto-generated method stub
-		return null;
+	public OracleAgent learn(final Assertion assertion) {
+		return this;
 	}
 
 	@Override
 	public double convergenceWith(final Agent other) {
-		// TODO Auto-generated method stub
-		return 0;
+		throw new UnsupportedOperationException("Cannot calculate convergence with oracles");
 	}
 
 	@Override
 	public double convergenceWith(final List<Concept> concepts) {
-		// TODO Auto-generated method stub
-		return 0;
+		throw new UnsupportedOperationException("Cannot calculate convergence with oracles");
 	}
 
 }
