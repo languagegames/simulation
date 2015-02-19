98229166844755da153394b82810707207c201a3
Paddy <pemcgovern@gmail.com>
2015-02-19 16:46:23 +0000

    GaussianObject creates list of objects from list of points.

--- src/main/java/conceptualspace/GaussianObject.java
+++ src/main/java/conceptualspace/GaussianObject.java
@@ -1,10 +1,14 @@
 package conceptualspace;
 
+import static conceptualspace.Point.mean;
+import static conceptualspace.Point.standardDeviation;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import utility.FakeRandom;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 public class GaussianObject implements PerceptualObject {
 
@@ -12,10 +16,26 @@ public class GaussianObject implements PerceptualObject {
 	private final List<Double> standardDeviation = new ArrayList<>();
 	private final Random random;
 
-	public GaussianObject(final List<Double> mean, final List<Double> standardDeviation, final FakeRandom random) {
+	public GaussianObject(final List<Double> mean, final List<Double> standardDeviation, final Random random) {
 		this.mean.addAll(mean);
 		this.standardDeviation.addAll(standardDeviation);
 		this.random = random;
+	}
+
+	public GaussianObject(final List<Double> mean, final List<Double> standardDeviation) {
+		this(mean, standardDeviation, new Random());
+	}
+
+	public static List<GaussianObject> makeListFrom(final List<Point> points, final int pointsPerObject) {
+		final int numObjects = points.size() / pointsPerObject;
+		final List<GaussianObject> objects = new ArrayList<>();
+		for (int i=0; i<numObjects; i++) {
+			final List<Point> objectPoints = points.subList(i*pointsPerObject, i*pointsPerObject+pointsPerObject);
+			final GaussianObject object = new GaussianObject(
+					mean(objectPoints).coordinates(), standardDeviation(objectPoints).coordinates());
+			objects.add(object);
+		}
+		return objects;
 	}
 
 	@Override
@@ -33,6 +53,32 @@ public class GaussianObject implements PerceptualObject {
 			coordinate = mean + standardDeviation*random.nextGaussian();
 		}
 		return coordinate;
+	}
+
+	@Override
+	public String toString() {
+		return "Gaussian object with mean " + mean.toString() + " and SD " + standardDeviation.toString();
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (obj instanceof GaussianObject) {
+			final GaussianObject other = (GaussianObject) obj;
+			return new EqualsBuilder()
+			.append(mean, other.mean)
+			.append(standardDeviation, other.standardDeviation)
+			.isEquals();
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder()
+		.append(mean)
+		.append(standardDeviation)
+		.toHashCode();
 	}
 
 }
--- src/test/java/conceptualspace/TestGaussianObject.java
+++ src/test/java/conceptualspace/TestGaussianObject.java
@@ -1,14 +1,40 @@
 package conceptualspace;
 
+import static conceptualspace.Point.mean;
+import static conceptualspace.Point.standardDeviation;
 import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
 import utility.FakeRandom;
 
 public class TestGaussianObject {
+
+	@Test
+	public void createsListOfObjectsFromListOfPoints() {
+		final List<Point> points = points(0.2, 0.3, 0.8, 0.5);
+
+		final GaussianObject object0 = new GaussianObject(
+				mean(points(0.2, 0.3)).coordinates(), standardDeviation(points(0.2, 0.3)).coordinates());
+		final GaussianObject object1 = new GaussianObject(
+				mean(points(0.8, 0.5)).coordinates(), standardDeviation(points(0.8, 0.5)).coordinates());
+
+		assertThat(GaussianObject.makeListFrom(points, 2), contains(object0, object1));
+	}
+
+	private List<Point> points(final double...values) {
+		final List<Point> points = new ArrayList<>();
+		for (int i=0; i<values.length; i++) {
+			points.add(new Point(values[i]));
+		}
+		return points;
+	}
 
 	@Test
 	public void doesNotProduceObservationsOutsideConceptualSpace() {
