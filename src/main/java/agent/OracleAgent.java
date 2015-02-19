01e04cf7c3232fe13132a4bba8ef32fb43410248
Paddy <pemcgovern@gmail.com>
2015-02-19 10:59:07 +0000

    Implement guess in OracleAgent.

--- src/main/java/agent/OracleAgent.java
+++ src/main/java/agent/OracleAgent.java
@@ -16,6 +16,17 @@ public class OracleAgent implements Agent {
 	}
 
 	@Override
+	public int guess(final List<PerceptualObject> guessingSet, final Assertion assertion) {
+		for (int i=0; i<guessingSet.size(); i++) {
+			final PerceptualObject object = guessingSet.get(i);
+			if (labelMapping.label(object) == assertion.label) {
+				return i;
+			}
+		}
+		return 0;
+	}
+
+	@Override
 	public OracleAgent incrementWeight(final double weightIncrement) {
 		return this;
 	}
@@ -48,12 +59,6 @@ public class OracleAgent implements Agent {
 	@Override
 	public double labelOverlap() {
 		throw new UnsupportedOperationException("Cannot calculate overlap in oracle agent.");
-	}
-
-	@Override
-	public int guess(final List<PerceptualObject> guessingSet, final Assertion assertion) {
-		// TODO Auto-generated method stub
-		return 0;
 	}
 
 }
--- src/test/java/agent/TestOracleAgent.java
+++ src/test/java/agent/TestOracleAgent.java
@@ -4,6 +4,9 @@ import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 import classifier.ExperimentData;
@@ -13,23 +16,42 @@ import conceptualspace.SimpleObject;
 
 public class TestOracleAgent {
 
-	private final PerceptualObject material0 = someMaterial(0.42);
-	private final PerceptualObject material1 = someMaterial(0.43);
-	private final PerceptualObject material2 = someMaterial(0.44);
-	private final ExperimentData data = new ExperimentData(
-			asList(material0, material1, material2), 0.42);
+	private final PerceptualObject object0 = someObject(0.42);
+	private final PerceptualObject object1 = someObject(0.43);
+	private final PerceptualObject object2 = someObject(0.44);
+	private final ExperimentData data = new ExperimentData(asList(object0, object1, object2), 0.42);
 	private final double someWeight = 0.42;
+
+	@Test
+	public void guessesZeroIfNoObjectMatchesAssertionLabel() {
+		final OracleAgent agent = new OracleAgent(new LabelMapping(data, asList(1, 2, 0)), someWeight);
+
+		final List<PerceptualObject> guessingSet = new ArrayList<>();
+		guessingSet.addAll(asList(object0, object1));
+
+		assertThat(agent.guess(guessingSet, new Assertion(object0, 0, someWeight)), equalTo(0));
+	}
+
+	@Test
+	public void guessesFirstObjectWhichMatchesAssertionLabel() {
+		final OracleAgent agent = new OracleAgent(new LabelMapping(data, asList(1, 2, 0)), someWeight);
+
+		final List<PerceptualObject> guessingSet = new ArrayList<>();
+		guessingSet.addAll(asList(object0, object1, object2));
+
+		assertThat(agent.guess(guessingSet, new Assertion(object0, 0, someWeight)), equalTo(2));
+	}
 
 	@Test
 	public void classifiesAccordingToPredefinedLabelMapping() {
 		final OracleAgent agent = new OracleAgent(new LabelMapping(data, asList(1, 2, 0)), someWeight);
 
-		assertThat(agent.assertion(material0), equalTo(new Assertion(material0, 1, someWeight)));
-		assertThat(agent.assertion(material1), equalTo(new Assertion(material1, 2, someWeight)));
-		assertThat(agent.assertion(material2), equalTo(new Assertion(material2, 0, someWeight)));
+		assertThat(agent.assertion(object0), equalTo(new Assertion(object0, 1, someWeight)));
+		assertThat(agent.assertion(object1), equalTo(new Assertion(object1, 2, someWeight)));
+		assertThat(agent.assertion(object2), equalTo(new Assertion(object2, 0, someWeight)));
 	}
 
-	private PerceptualObject someMaterial(final double d) {
+	private PerceptualObject someObject(final double d) {
 		return new SimpleObject(new Point(d));
 	}
 
