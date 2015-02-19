9551510b9d58b5b1d6436dadef3817c83a8cc192
Paddy <pemcgovern@gmail.com>
2015-02-19 14:57:32 +0000

    nextDouble method in FakeRandom.

--- src/test/java/utility/FakeRandom.java
+++ src/test/java/utility/FakeRandom.java
@@ -10,9 +10,19 @@ public class FakeRandom extends Random {
 
 	private static final long serialVersionUID = 1L;
 	private final List<Integer> integers = new ArrayList<>();
+	private final List<Double> doubles = new ArrayList<>();
 
 	public FakeRandom(final Integer...integers) {
 		this.integers.addAll(asList(integers));
+	}
+
+	public FakeRandom(final Double...doubles) {
+		this.doubles.addAll(asList(doubles));
+	}
+
+	@Override
+	public double nextDouble() {
+		return doubles.remove(0);
 	}
 
 	@Override
