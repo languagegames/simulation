ca13cb0f649f83e732c6f058b7281c656f9081bd
Paddy <pemcgovern@gmail.com>
2015-02-20 17:06:51 +0000

    Make mean and standard deviation calculations in TimeSeries private.

--- src/main/java/languagegames/analysis/TimeSeries.java
+++ src/main/java/languagegames/analysis/TimeSeries.java
@@ -35,36 +35,36 @@ public class TimeSeries {
 	public static TimeSeriesAverage average(final List<TimeSeries> results) {
 		return new TimeSeriesAverage(
 				results.get(0).timeSteps,
-				mean(results).values,
-				standardDeviation(results).values);
+				mean(results),
+				standardDeviation(results));
 	}
 
-	public static TimeSeries standardDeviation(final List<TimeSeries> results) {
-		final TimeSeries mean = mean(results);
+	private static List<Double> standardDeviation(final List<TimeSeries> results) {
+		final List<Double> mean = mean(results);
 		final List<Double> standardDeviation = new ArrayList<>();
 		final int numTimeSteps = results.get(0).values.size();
 		for (int timeStep=0; timeStep<numTimeSteps; timeStep++) {
 			standardDeviation.add(standardDeviation(results, mean, timeStep));
 		}
-		return new TimeSeries(standardDeviation, results.get(0).timeSteps);
+		return standardDeviation;
 	}
 
 	private static double standardDeviation(
-			final List<TimeSeries> results, final TimeSeries mean, final int dimension) {
+			final List<TimeSeries> results, final List<Double> mean, final int dimension) {
 		double sumOfSquares = 0;
 		for (final TimeSeries timeSeries : results) {
-			sumOfSquares += pow(timeSeries.values.get(dimension) - mean.values.get(dimension), 2);
+			sumOfSquares += pow(timeSeries.values.get(dimension) - mean.get(dimension), 2);
 		}
 		return sqrt(sumOfSquares / (results.size()-1));
 	}
 
-	public static TimeSeries mean(final List<TimeSeries> results) {
+	private static List<Double> mean(final List<TimeSeries> results) {
 		final List<Double> mean = new ArrayList<>();
 		final int numTimeSteps = results.get(0).values.size();
 		for (int timeStep=0; timeStep<numTimeSteps; timeStep++) {
 			mean.add(mean(results, timeStep));
 		}
-		return new TimeSeries(mean, results.get(0).timeSteps);
+		return mean;
 	}
 
 	private static double mean(final List<TimeSeries> results, final int timeStep) {
--- src/test/java/languagegames/analysis/TestTimeSeries.java
+++ src/test/java/languagegames/analysis/TestTimeSeries.java
@@ -11,19 +11,15 @@ import org.junit.Test;
 public class TestTimeSeries {
 
 	@Test
-	public void calculatesStandardDeviationFromList() {
+	public void calculatesMeanAndStandardDeviationFromList() {
 		final List<TimeSeries> results = asList(
 				new TimeSeries(0.2, 0.5), new TimeSeries(0.9, 0.1), new TimeSeries(0.4, 0.3));
 
-		assertThat(TimeSeries.standardDeviation(results), equalTo(new TimeSeries(0.36055512754639896, 0.2)));
-	}
-
-	@Test
-	public void calculatesMeanFromList() {
-		final List<TimeSeries> results = asList(
-				new TimeSeries(0.2, 0.5), new TimeSeries(0.9, 0.1), new TimeSeries(0.4, 0.3));
-
-		assertThat(TimeSeries.mean(results), equalTo(new TimeSeries(0.5, 0.3)));
+		assertThat(TimeSeries.average(results), equalTo(
+				new TimeSeriesAverage(
+						asList(0, 1),
+						asList(0.5, 0.3),
+						asList(0.36055512754639896, 0.2))));
 	}
 
 }
