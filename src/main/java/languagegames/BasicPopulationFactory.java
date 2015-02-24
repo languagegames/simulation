30f4cbb434638fefc4fd74a285df337edc1690c1
Paddy <pemcgovern@gmail.com>
2015-02-24 16:38:32 +0000

    Create BasicPopulationFactory.

---
+++ src/main/java/languagegames/BasicPopulationFactory.java
@@ -0,0 +1,47 @@
+package languagegames;
+
+import static agent.Concept.randomConcept;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import agent.Agent;
+import agent.BasicAgent;
+import agent.Concept;
+
+public class BasicPopulationFactory {
+
+	private final Random random;
+
+	public BasicPopulationFactory() {
+		random = new Random();
+	}
+
+	public BasicPopulation createRandom(
+			final int numAgents,
+			final int numDimensions,
+			final double minThreshold,
+			final double maxThreshold,
+			final ObjectPool objectPool) {
+		final List<Agent> agents = new ArrayList<>();
+		for (double i=0; i<numAgents; i++) {
+			final double weight = i/numAgents;
+			agents.add(randomAgent(numDimensions, minThreshold, maxThreshold, weight));
+		}
+		return new BasicPopulation(agents, objectPool, new RandomPairer());
+	}
+
+	private BasicAgent randomAgent(
+			final int numDimensions,
+			final double minThreshold,
+			final double maxThreshold,
+			final double weight) {
+		final List<Concept> concepts = new ArrayList<>();
+		for (int i=0; i<10; i++) {
+			concepts.add(randomConcept(numDimensions, minThreshold, maxThreshold, random));
+		}
+		return new BasicAgent(weight, concepts);
+	}
+
+}
