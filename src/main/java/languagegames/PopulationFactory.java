bb3beabd752e63221e1c38aa7258d8df76440ac5
Paddy <pemcgovern@gmail.com>
2015-03-03 11:34:23 +0000

    Add NegatingAgent method to PopulationFactory.

--- src/main/java/languagegames/PopulationFactory.java
+++ src/main/java/languagegames/PopulationFactory.java
@@ -9,8 +9,38 @@ import java.util.Random;
 import agent.Agent;
 import agent.BasicAgent;
 import agent.Concept;
+import agent.NegatingAgent;
 
 public class PopulationFactory {
+
+	public static Population negatingPopulation(
+			final double positivePrior,
+			final int numAgents,
+			final int numDimensions,
+			final double initialThreshold,
+			final ObjectPool objectPool)
+	{
+		final List<Agent> agents = new ArrayList<>();
+		for (double i=0; i<numAgents; i++) {
+			final double weight = i/numAgents;
+			agents.add(negatingAgent(positivePrior, numDimensions, initialThreshold, weight));
+		}
+		return new BasicPopulation(agents, objectPool, new RandomPairer());
+	}
+
+	private static Agent negatingAgent(
+			final double positivePrior,
+			final int numDimensions,
+			final double initialThreshold,
+			final double weight)
+	{
+		final List<Concept> concepts = new ArrayList<>();
+		final Random random = new Random();
+		for (int i=0; i<10; i++) {
+			concepts.add(randomConcept(numDimensions, initialThreshold, random));
+		}
+		return new NegatingAgent(positivePrior, weight, concepts);
+	}
 
 	public static OraclePopulation oraclePopulation(
 			final List<Agent> oracles,
