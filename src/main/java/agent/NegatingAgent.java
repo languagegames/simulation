c31f73b18c4ee80e781264d98bfddfe11d594f04
Paddy <pemcgovern@gmail.com>
2015-03-03 10:46:45 +0000

    Create NegatingAgent, implement assertions.

---
+++ src/main/java/agent/NegatingAgent.java
@@ -0,0 +1,180 @@
+package agent;
+
+import static agent.Assertion.negativeAssertion;
+import static agent.Assertion.positiveAssertion;
+import static java.util.Arrays.asList;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import conceptualspace.PerceptualObject;
+import conceptualspace.Point;
+
+public class NegatingAgent implements Agent {
+
+	private final List<Concept> concepts = new ArrayList<>();
+	private final double positivePrior;
+	private final double weight;
+
+	public NegatingAgent(final double positivePrior, final double weight, final List<Concept> concepts) {
+		this.positivePrior = positivePrior;
+		this.weight = weight;
+		this.concepts.addAll(concepts);
+	}
+
+	public NegatingAgent(final double positivePrior, final double weight, final Concept...concepts) {
+		this(positivePrior, weight, asList(concepts));
+	}
+
+	@Override
+	public int guess(final List<PerceptualObject> guessingSet, final Assertion assertion) {
+		final Concept assertionConcept = concepts.get(assertion.label);
+		int guess = 0; double highestAppropriateness = 0;
+		for (final PerceptualObject object : guessingSet) {
+			final double appropriateness = assertionConcept.appropriatenessOf(object.observation());
+			if (appropriateness > highestAppropriateness) {
+				guess = guessingSet.indexOf(object);
+				highestAppropriateness = appropriateness;
+			}
+		}
+		return guess;
+	}
+
+	@Override
+	public double labelOverlap() {
+		double sum = 0;
+		final int numberOfPairs = concepts.size()*(concepts.size()-1)/2;
+		for (int i = 0; i < concepts.size()-1; i++) {
+			for (int j = i+1; j < concepts.size(); j++) {
+				sum += concepts.get(i).overlapWith(concepts.get(j));
+			}
+		}
+		return sum / numberOfPairs;
+	}
+
+	@Override
+	public Agent incrementWeight(final double weightIncrement) {
+		double newWeight = weight + weightIncrement;
+		if (newWeight > 0.9) {
+			newWeight = 0.1;
+		}
+		return new NegatingAgent(positivePrior, newWeight, concepts);
+	}
+
+	@Override
+	public double weight() {
+		return weight;
+	}
+
+	@Override
+	public double convergenceWith(final List<Concept> concepts) {
+		double result = 0;
+		for (int i = 0; i < concepts.size(); i++) {
+			result += this.concepts.get(i).hausdorffDistanceFrom(concepts.get(i)) / concepts.size();
+		}
+		return result;
+	}
+
+	@Override
+	public double convergenceWith(final Agent other) {
+		return other.convergenceWith(concepts);
+	}
+
+	@Override
+	public Assertion assertion(final PerceptualObject object) {
+		final Point observation = object.observation();
+		final List<Concept> sortedConcepts = sortConcepts(observation);
+		if (positiveProbability(sortedConcepts, observation) >= negativeProbability(sortedConcepts, observation)) {
+			return positiveAssertion(object, concepts.indexOf(sortedConcepts.get(0)), weight);
+		} else {
+			return negativeAssertion(object, concepts.indexOf(sortedConcepts.get(sortedConcepts.size()-1)), weight);
+		}
+	}
+
+	private double positiveProbability(final List<Concept> concepts, final Point observation) {
+		final int k = concepts.size(), n = this.concepts.size();
+		final double p = positivePrior, q = 1 - positivePrior;
+		final List<Double> appropriatenessDifferences = appropriatenessDifferences(concepts, observation);
+		appropriatenessDifferences.add(concepts.get(k-1).appropriatenessOf(observation));
+		double result = 0;
+		for (int i=0; i<k; i++) {
+			final double coefficient = p / ((i+1)*p + (n-(i+1))*q);
+			result += appropriatenessDifferences.get(i) * coefficient;
+		}
+		return result;
+	}
+
+	private double negativeProbability(final List<Concept> concepts, final Point observation) {
+		final int k = concepts.size(), n = this.concepts.size();
+		final double p = positivePrior, q = 1 - positivePrior;
+		final List<Double> appropriatenessDifferences = appropriatenessDifferences(concepts, observation);
+		appropriatenessDifferences.add(0, 1 - concepts.get(0).appropriatenessOf(observation));
+		double result = 0;
+		for (int i=0; i<k; i++) {
+			final double coefficient = q / (i*p + (n-i)*q);
+			result += appropriatenessDifferences.get(i) * coefficient;
+		}
+		return result;
+	}
+
+	private List<Double> appropriatenessDifferences(final List<Concept> concepts, final Point observation) {
+		final List<Double> appropriatenessDifferences = new ArrayList<>();
+		for (int i=0; i<concepts.size()-1; i++) {
+			final double a = concepts.get(i).appropriatenessOf(observation);
+			final double b = concepts.get(i+1).appropriatenessOf(observation);
+			appropriatenessDifferences.add(a - b);
+		}
+		return appropriatenessDifferences;
+	}
+
+	private List<Concept> sortConcepts(final Point observation) {
+		final List<Concept> concepts = new ArrayList<>(this.concepts);
+		Collections.sort(concepts, new Comparator<Concept>() {
+	        @Override public int compare(final Concept c1, final Concept c2) {
+	            return c1.appropriatenessOf(observation) - c2.appropriatenessOf(observation) > 0 ? -1 : 1;
+	        }
+	    });
+		removeZeroAppropriatenesses(concepts, observation);
+		return concepts;
+	}
+
+	private void removeZeroAppropriatenesses(final List<Concept> concepts, final Point observation) {
+		for (final Iterator<Concept> iterator = concepts.iterator(); iterator.hasNext();) {
+		    final Concept concept = iterator.next();
+		    if (concept.appropriatenessOf(observation) == 0) {
+		        iterator.remove();
+		    }
+		}
+	}
+
+	@Override
+	public Agent learn(final Assertion assertion) {
+		final Concept toUpdate = concepts.get(assertion.label);
+		final Concept updated = toUpdate.update(assertion);
+		final List<Concept> newConcepts = new ArrayList<>(concepts);
+		newConcepts.set(assertion.label, updated);
+		return new NegatingAgent(positivePrior, weight, newConcepts);
+	}
+
+	@Override
+	public String toString() {
+		return "Negating agent with " + concepts.toString();
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+}
---
+++ src/test/java/agent/TestNegatingAgent.java
@@ -0,0 +1,116 @@
+package agent;
+
+import static agent.Assertion.negativeAssertion;
+import static agent.Assertion.positiveAssertion;
+import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import conceptualspace.PerceptualObject;
+import conceptualspace.Point;
+import conceptualspace.SimpleObject;
+
+public class TestNegatingAgent {
+
+	private final double somePrior = 0.42;
+	private final double someWeight = 0.42;
+
+	@Test
+	public void assertionDependsOnPositivePrior() {
+		final List<Concept> concepts = asList(
+				new Concept(new Point(0.8), 0.6),
+				new Concept(new Point(0.2), 0.5),
+				new Concept(new Point(0.9), 0.7),
+				new Concept(new Point(0.4), 0.4),
+				new Concept(new Point(0.2), 0.2));
+
+		final NegatingAgent anAgent = new NegatingAgent(0.5, someWeight, concepts);
+		final NegatingAgent anotherAgent = new NegatingAgent(0.4, someWeight, concepts);
+
+		final PerceptualObject object = new SimpleObject(new Point(0.5));
+
+		assertThat(anAgent.assertion(object), equalTo(positiveAssertion(object, 3, someWeight)));
+		assertThat(anotherAgent.assertion(object), equalTo(negativeAssertion(object, 1, someWeight)));
+	}
+
+	@Test
+	public void guessesMostAppropriateObjectGivenAssertionConcept() {
+		final PerceptualObject mostAppropriate = object(0.6);
+		final PerceptualObject other = object(0.5);
+		final Concept concept0 = new Concept(new Point(0.7), 0.9);
+		final NegatingAgent agent = new NegatingAgent(somePrior, someWeight, concept0);
+
+		final List<PerceptualObject> guessingSet = new ArrayList<>();
+		guessingSet.addAll(asList(other, mostAppropriate));
+
+		assertThat(agent.guess(guessingSet, new Assertion(mostAppropriate, 0, someWeight)), equalTo(1));
+	}
+
+	@Test
+	public void calculatesOverlapAcrossAllPairsOfConcepts() {
+		final Concept concept0 = new Concept(new Point(0.7), 0.9);
+		final Concept concept1 = new Concept(new Point(0.5), 0.5);
+		final Concept concept2 = new Concept(new Point(0.4), 0.2);
+		final NegatingAgent agent = new NegatingAgent(somePrior, someWeight, concept0, concept1, concept2);
+
+		final double averageOverlap =
+				(overlap(concept0, concept1) + overlap(concept0, concept2) + overlap(concept1, concept2)) / 3;
+
+		assertThat(agent.labelOverlap(), equalTo(averageOverlap));
+	}
+
+	private double overlap(final Concept concept, final Concept other) {
+		return concept.overlapWith(other);
+	}
+
+	@Test
+	public void incrementsWeight() {
+		final NegatingAgent agent = new NegatingAgent(somePrior, 0.5, new ArrayList<Concept>());
+		final Agent newAgent = agent.incrementWeight(0.1);
+		assertThat(newAgent.weight(), equalTo(0.6));
+	}
+
+	@Test
+	public void getsConvergenceFromPairsOfCorrespondingConcepts() {
+		final NegatingAgent agent = new NegatingAgent(
+				somePrior,
+				someWeight,
+				new Concept(new Point(0.2), 0.7),
+				new Concept(new Point(0.5), 0.5));
+		final Agent other = new NegatingAgent(
+				somePrior,
+				someWeight,
+				new Concept(new Point(0.4), 0.4),
+				new Concept(new Point(0.7), 0.8));
+
+		assertThat(agent.convergenceWith(other), equalTo(0.35));
+	}
+
+	@Test
+	public void learnsByUpdatingAppropriateConcept() {
+		final NegatingAgent agent = new NegatingAgent(
+				somePrior,
+				someWeight,
+				new Concept(new Point(0.42), 0.42),
+				new Concept(new Point(0.5), 0.5));
+
+		final Agent updatedAgent = new NegatingAgent(
+				somePrior,
+				someWeight,
+				new Concept(new Point(0.42), 0.42),
+				new Concept(new Point(0.74), 0.8));
+
+		assertThat(agent.learn(new Assertion(new SimpleObject(new Point(0.9)), 1, 0.8)),
+				equalTo(updatedAgent));
+	}
+
+	private SimpleObject object(final double d) {
+		return new SimpleObject(new Point(d));
+	}
+
+}
