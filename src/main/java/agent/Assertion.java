f3b5b0c4ec3f9b671e29939c2eef284fceb3be20
Paddy <pemcgovern@gmail.com>
2015-03-03 09:51:31 +0000

    Assertions can be negative.

--- src/main/java/agent/Assertion.java
+++ src/main/java/agent/Assertion.java
@@ -7,14 +7,28 @@ import conceptualspace.PerceptualObject;
 
 public class Assertion {
 
+	public static Assertion positiveAssertion(final PerceptualObject object, final int label, final double weight) {
+		return new Assertion(object, label, weight, true);
+	}
+
+	public static Assertion negativeAssertion(final PerceptualObject object, final int label, final double weight) {
+		return new Assertion(object, label, weight, false);
+	}
+
 	public final PerceptualObject object;
 	public final int label;
 	public final double weight;
+	public final boolean isPositive;
 
-	public Assertion(final PerceptualObject object, final int label, final double weight) {
+	public Assertion(final PerceptualObject object, final int label, final double weight, final boolean isPositive) {
 		this.object = object;
 		this.label = label;
 		this.weight = weight;
+		this.isPositive = isPositive;
+	}
+
+	public Assertion(final PerceptualObject object, final int label, final double weight) {
+		this(object, label, weight, true);
 	}
 
 	public boolean matches(final Assertion other) {
@@ -23,6 +37,9 @@ public class Assertion {
 
 	@Override
 	public String toString() {
+		if (!isPositive) {
+			return "Assertion: " + object.toString() + " is NOT " + label + ", weight " + weight;
+		}
 		return "Assertion: " + object.toString() + " is " + label + ", weight " + weight;
 	}
 
