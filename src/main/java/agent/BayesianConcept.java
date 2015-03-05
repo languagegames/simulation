401a8c63ce2a54ee88c44fabc98eac86c4bc8ed5
Paddy <pemcgovern@gmail.com>
2015-03-05 10:03:40 +0000

    BayesianConcept has randomConcept method.

--- src/main/java/agent/BayesianConcept.java
+++ src/main/java/agent/BayesianConcept.java
@@ -10,6 +10,7 @@ import static java.util.Arrays.asList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
@@ -30,6 +31,21 @@ public class BayesianConcept {
 
 	public BayesianConcept(final Point...points) {
 		this(asList(points));
+	}
+
+	public static BayesianConcept randomConcept(final int numDimensions, final Random random) {
+		final List<Point> points = new ArrayList<>();
+		points.add(randomPoint(numDimensions, random));
+		points.add(randomPoint(numDimensions, random));
+		return new BayesianConcept(points);
+	}
+
+	private static Point randomPoint(final int numDimensions, final Random random) {
+		final List<Double> coordinates = new ArrayList<>();
+		for (int i=0; i<numDimensions; i++) {
+			coordinates.add(random.nextDouble());
+		}
+		return new Point(coordinates);
 	}
 
 	public BayesianConcept update(final Point point) {
