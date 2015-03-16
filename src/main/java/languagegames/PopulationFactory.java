86a23d986066783224b0533979ba41b3364f0193
Paddy <pemcgovern@gmail.com>
2015-03-16 14:37:01 +0000

    Add max likelihood population to PopulationFactory.

--- src/main/java/languagegames/PopulationFactory.java
+++ src/main/java/languagegames/PopulationFactory.java
@@ -9,8 +9,31 @@ import agent.BasicAgent;
 import agent.BayesianConcept;
 import agent.Concept;
 import agent.FuzzyConcept;
+import agent.MaxLikelihoodConcept;
 
 public class PopulationFactory {
+
+	public static Population maxLikelihoodPopulation(
+			final int numAgents,
+			final int numDimensions,
+			final ObjectPool objectPool)
+	{
+		final List<Agent> agents = new ArrayList<>();
+		for (double i=0; i<numAgents; i++) {
+			final double weight = i/numAgents;
+			agents.add(maxLikelihoodAgent(numDimensions, weight));
+		}
+		return new BasicPopulation(agents, objectPool, new RandomPairer());
+	}
+
+	private static Agent maxLikelihoodAgent(final int numDimensions, final double weight) {
+		final List<Concept> concepts = new ArrayList<>();
+		final Random random = new Random();
+		for (int i=0; i<10; i++) {
+			concepts.add(MaxLikelihoodConcept.randomConcept(numDimensions, random));
+		}
+		return new BasicAgent(weight, concepts);
+	}
 
 	public static BasicPopulation bayesianPopulation(
 			final int numAgents,
