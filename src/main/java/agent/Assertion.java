fafa507ff9c0114efe7c1c694673593e19876c69
Paddy <pemcgovern@gmail.com>
2015-03-18 11:46:49 +0000

    Add labels list field to Assertion.

--- src/main/java/agent/Assertion.java
+++ src/main/java/agent/Assertion.java
@@ -1,5 +1,10 @@
 package agent;
 
+import static java.util.Arrays.asList;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
@@ -8,13 +13,31 @@ import conceptualspace.PerceptualObject;
 public class Assertion {
 
 	public final PerceptualObject object;
+	private final List<Integer> labels = new ArrayList<>();
 	public final int label;
 	public final double weight;
 
-	public Assertion(final PerceptualObject object, final int label, final double weight) {
+	private Assertion(final PerceptualObject object, final List<Integer> labels, final double weight) {
 		this.object = object;
-		this.label = label;
+		this.labels.addAll(labels);
+		label = labels.get(0);
 		this.weight = weight;
+	}
+
+	public Assertion(final PerceptualObject object, final int label, final double weight) {
+		this(object, asList(label), weight);
+	}
+
+	public Assertion(final PerceptualObject object, final int label) {
+		this(object, asList(label), 0);
+	}
+
+	public Assertion(final PerceptualObject object, final int label0, final int label1) {
+		this(object, asList(label0, label1), 0);
+	}
+
+	public List<Integer> labels() {
+		return new ArrayList<>(labels);
 	}
 
 	public boolean matches(final Assertion other) {
