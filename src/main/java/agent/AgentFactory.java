49f4259711172adfae5405493d564fbe4e6f9654
Paddy <pemcgovern@gmail.com>
2015-03-19 09:30:20 +0000

    Create AgentFactory.

---
+++ src/main/java/agent/AgentFactory.java
@@ -0,0 +1,35 @@
+package agent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class AgentFactory {
+
+	public static BasicAgent randomAgent(
+			final int numDimensions,
+			final double initialThreshold,
+			final double weight,
+			final AssertionModel assertionModel)
+	{
+		return agent()
+				.withConcepts(randomConcepts(numDimensions, initialThreshold))
+				.withWeight(weight)
+				.withAssertionModel(assertionModel)
+				.build();
+	}
+
+	private static List<Concept> randomConcepts(final int numDimensions, final double initialThreshold) {
+		final Random random = new Random();
+		final List<Concept> concepts = new ArrayList<>();
+		for (int i=0; i<10; i++) {
+			concepts.add(FuzzyConcept.randomConcept(numDimensions, initialThreshold, random));
+		}
+		return concepts;
+	}
+
+	private static BasicAgentBuilder agent() {
+		return new BasicAgentBuilder();
+	}
+
+}
