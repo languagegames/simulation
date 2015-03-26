e575c4ee78d26013269d442b4a66bcbdbe717ccb
Paddy <pemcgovern@gmail.com>
2015-03-26 11:25:12 +0000

    Create ClassificationExperiment.

---
+++ src/main/java/classifier/ClassificationExperiment.java
@@ -0,0 +1,24 @@
+package classifier;
+
+import java.util.List;
+
+import agent.Agent;
+import conceptualspace.PerceptualObject;
+
+public class ClassificationExperiment {
+
+	public ClassificationExperiment(
+			final Agent pupil,
+			final Agent teacher,
+			final List<PerceptualObject> data,
+			final int crossValFactor)
+	{
+		// TODO Auto-generated constructor stub
+	}
+
+	public Double score() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
