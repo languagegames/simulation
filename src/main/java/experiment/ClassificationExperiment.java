2c6ee5e6064952ed6c93294c2bce9db934eca3a5
Paddy <pemcgovern@gmail.com>
2015-03-27 15:49:40 +0000

    Create ClassificationExperiment.

---
+++ src/main/java/experiment/ClassificationExperiment.java
@@ -0,0 +1,64 @@
+package experiment;
+
+import static agent.BasicAgent.randomAgent;
+import static utility.ResultsPrinter.print;
+import static utility.Stats.mean;
+import static utility.Stats.standardDeviation;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import utility.DataReader;
+import agent.Agent;
+import agent.LabelMapping;
+import agent.OracleAgent;
+import agent.assertions.BasicAssertionModel;
+import agent.concept.RandomConceptFactory;
+import conceptualspace.PerceptualObject;
+import conceptualspace.SimpleObject;
+
+public class ClassificationExperiment {
+
+	private final String fileID;
+	private final int numRuns;
+	private final int crossValFactor;
+	private final Agent pupil;
+	private final Agent teacher;
+	private List<PerceptualObject> data = new ArrayList<>();
+
+	public ClassificationExperiment(
+			final String fileID,
+			final int numDimensions,
+			final int numLabels,
+			final int numRuns,
+			final int crossValFactor,
+			final RandomConceptFactory conceptFactory,
+			final String dataFile,
+			final String labelsFile)
+	{
+		this.fileID = fileID;
+		this.numRuns = numRuns;
+		this.crossValFactor = crossValFactor;
+
+		pupil = randomAgent(numDimensions, numLabels, 0, new BasicAssertionModel(), conceptFactory);
+
+		final DataReader dataReader = new DataReader();
+		data = SimpleObject.makeListFrom(dataReader.points(dataFile, numDimensions));
+		teacher = new OracleAgent(new LabelMapping(data, dataReader.integers(labelsFile)), 0.95);
+
+	}
+
+	public void run() {
+		final List<Double> scores = new ArrayList<>();
+		for (int i=0; i<numRuns; i++) {
+			Collections.shuffle(data);
+			final ClassificationCrossValidation crossValidation =
+					new ClassificationCrossValidation(pupil, teacher, data, crossValFactor);
+			scores.add(crossValidation.score());
+		}
+
+		print(mean(scores) + ", " + standardDeviation(scores), fileID);
+	}
+
+}
