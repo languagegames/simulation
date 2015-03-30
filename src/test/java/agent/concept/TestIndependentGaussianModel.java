15aff1edf70c4a3142272f18051405d828822354
Paddy <pemcgovern@gmail.com>
2015-03-30 15:09:50 +0100

    Refactor test.

--- src/test/java/agent/concept/TestIndependentGaussianModel.java
+++ src/test/java/agent/concept/TestIndependentGaussianModel.java
@@ -9,21 +9,18 @@ import java.util.List;
 
 import org.junit.Test;
 
-import agent.assertions.Assertion;
 import conceptualspace.Point;
-import conceptualspace.SimpleObject;
 
 public class TestIndependentGaussianModel {
 
 	@Test
 	public void stores20PointsAtMost() {
 		final Point point = new Point(0.42);
-		final Assertion assertion = new Assertion(new SimpleObject(point), 42, 0.42);
 		IndependentGaussianModel concept = new IndependentGaussianModel(point, point);
 		final List<Point> points = new ArrayList<>();
 		for (int i=0; i<20; i++) {
 			points.add(point);
-			concept = concept.update(assertion.object.observation());
+			concept = concept.update(point);
 		}
 		assertThat(concept, equalTo(new IndependentGaussianModel(points)));
 	}
@@ -34,8 +31,7 @@ public class TestIndependentGaussianModel {
 		final Point point1 = new Point(0.1);
 		final Point point2 = new Point(0.2);
 		final IndependentGaussianModel concept = new IndependentGaussianModel(point1, point0);
-		final Assertion assertion = new Assertion(new SimpleObject(point2), 42, 0.42);
-		assertThat(concept.update(assertion.object.observation()), equalTo(new IndependentGaussianModel(point2, point1, point0)));
+		assertThat(concept.update(point2), equalTo(new IndependentGaussianModel(point2, point1, point0)));
 	}
 
 	@Test
