7a28a56f73db5e0408e16aef20585038b5a066a8
Paddy <pemcgovern@gmail.com>
2015-03-30 15:50:25 +0100

    Create new concept factories.

---
+++ src/main/java/agent/concept/MultivariateMaxLikelihoodFactory.java
@@ -0,0 +1,22 @@
+package agent.concept;
+
+import java.util.Random;
+
+import utility.matrix.Matrix;
+
+public class MultivariateMaxLikelihoodFactory implements RandomConceptFactory {
+
+	private final Random random = new Random();
+
+	@Override
+	public Concept randomConcept(final int numDimensions) {
+		final double[][] vals = new double[numDimensions+1][numDimensions];
+		for (int i=0; i<numDimensions+1; i++) {
+			for (int j=0; j<numDimensions; j++) {
+				vals[i][j] = random.nextDouble();
+			}
+		}
+		return new MaxLikelihoodConcept(new MultivariateGaussianModel(new Matrix(vals)));
+	}
+
+}
