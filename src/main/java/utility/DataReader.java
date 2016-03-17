a236e1783ce47255107aab828ddbd0d2c1b47f80
Paddy <pemcgovern@gmail.com>
2016-03-17 13:07:17 +0000

    DataReader accesses stream rather than file.

--- src/main/java/utility/DataReader.java
+++ src/main/java/utility/DataReader.java
@@ -2,12 +2,12 @@ package utility;
 
 import conceptualspace.Point;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 import static java.util.Arrays.asList;
 
@@ -35,7 +35,7 @@ public class DataReader {
 
     public static List<Integer> integers(final String fileName) {
         final List<String> lines = lines(fileName);
-        final List<Integer> integers = new ArrayList<Integer>();
+        final List<Integer> integers = new ArrayList<>();
         for (final String line : lines) {
             integers.addAll(lineInts(line));
         }
@@ -44,25 +44,20 @@ public class DataReader {
 
     private static List<Integer> lineInts(final String line) {
         final List<String> strings = asList(line.split(","));
-        final List<Integer> integers = new ArrayList<Integer>();
-        for (final String string : strings) {
-            integers.add(Integer.valueOf(string));
-        }
-        return integers;
+        return strings.stream()
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
     }
 
     public static List<Point> points(final String fileName, final int numDimensions) {
-        final List<String> lines = lines(fileName);
-        final List<Point> points = new ArrayList<Point>();
-        for (final String line : lines) {
-            points.add(point(line, numDimensions));
-        }
-        return points;
+        return lines(fileName).stream()
+                .map(line -> point(line, numDimensions))
+                .collect(Collectors.toList());
     }
 
     private static Point point(final String line, final int numDimensions) {
         final List<String> strings = asList(line.split(","));
-        final List<Double> coordinates = new ArrayList<Double>();
+        final List<Double> coordinates = new ArrayList<>();
         for (int i = 0; i < numDimensions; i++) {
             coordinates.add(Double.valueOf(strings.get(i)));
         }
@@ -86,15 +81,10 @@ public class DataReader {
 
     @SuppressWarnings("ConstantConditions")
     private static List<String> read(final String fileName) {
-        final List<String> result = new ArrayList<String>();
+        final List<String> result = new ArrayList<>();
         final ClassLoader classLoader = DataReader.class.getClassLoader();
-        final File file = new File(classLoader.getResource(fileName).getFile());
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        final InputStream stream = classLoader.getResourceAsStream(fileName);
+        Scanner scanner = new Scanner(stream);
         while (scanner.hasNextLine()) {
             final String line = scanner.nextLine();
             result.add(line);
